d1197d5b52afc7da6d0e31a306b0d2a9cfd02a61
Marcos Castro <marcos.yk07@gmail.com>
2026-08-16 18:55:40 -0300

    feat: add global styles and fots

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './styles/global.css'
+
+function MyName () {
+  return (
+    <h2>Marcos Castro</h2>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <MyName />
+    </>
+  )
+}
+
+export default App
